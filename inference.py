6d4395971826c0ef54795d3ed8aeecdeaac63e7e
ThanhNS <sthanhng@gmail.com>
2018-10-17 08:47:32 +0700

    fix some issues

--- inference.py
+++ inference.py
@@ -1,5 +1,4 @@
 import argparse
-import pickle
 
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
@@ -10,8 +9,6 @@ from utils import *
 parse = argparse.ArgumentParser()
 parse.add_argument('--model-name', required=True,
                    help='path to trained model model')
-parse.add_argument('--label-bin', required=True,
-                   help='path to label binarizer')
 parse.add_argument('--sample', required=True,
                    help='path to input sample')
 args = parse.parse_args()
@@ -30,21 +27,22 @@ image = np.expand_dims(image, axis=0)
 
 
 #####################################################################
-# load the trained model and the label binarizer
+# load the trained model
 print('==> Loading network...')
 model = load_model(args.model_name)
-lb = pickle.loads(open(args.label_bin, 'rb').read())
 
 # classify the input image
 print('==> Classifying image...')
-proba = model.predict(image)[0]
-idx = np.argmax(proba)
-label = lb.classes_[idx]
+confidence = model.predict(image)[0]
+idx = np.argmax(confidence)
+
+classes = ['cat', 'dog']
+label = classes[idx]
 
 filename = args.sample[args.sample.rfind(os.path.sep) + 1:]
 
 # build the label and draw the label on the image
-text = '{}: {:.2f}%'.format(label, proba[idx] * 100)
+text = '{}: {:.2f}%'.format(label, confidence[idx] * 100)
 output = resize(output, width=400)
 cv2.putText(output, text, (10, 25), cv2.FONT_HERSHEY_SIMPLEX,
             0.6, COLOR_YELLOW, 1)
